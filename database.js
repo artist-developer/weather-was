b095c6318ae0d277cf63d5926d7e7475cc2f1fbe
Artist-developer <artist.develops@gmail.com>
2021-02-13 22:21:24 +0900

    add mysql

---
+++ database.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'db-5nspr-fkr.cdb.fin-ntruss.com',
+    port: '3306',
+    user: 'youngin0108',
+    password: '!qlalf7431',
+    database: 'db'
+});
+
+conn.connect();
+
+conn.query('SELECT * FROM weatherTable', function*(err, results, fields){
+    if(err){
+        console.log(err);
+    }
+    console.log(results);
+})
+
+conn.end();
--- index.js
+++ index.js
@@ -2,21 +2,28 @@ var app = require("express")();
 var http = require("http").createServer(app);
 var axios = require("axios");
 var cors = require("cors");
+
 const API_KEY = "3b5b2c5a5d90ef09d98c467547d79f52";
 const region = "Seoul";
+
 app.use(cors());
+
 app.use("/get-weather", async function (req, res) {
-  console.log("in");
+
+  console.log("get-weather");
+
   const result = await axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=${region}&APPID=${API_KEY}&units=metric`
   );
 
   res.send(result.data);
+  
 });
 
 app.use("/", function(req, res) {
   res.send("HTTP 200 Ok");
 });
+
 // 서버리슨
 http.listen(8080, () => {
   console.log("Server listening on port 8080");
